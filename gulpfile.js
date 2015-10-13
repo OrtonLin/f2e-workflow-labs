a61a5c4d98beff113ef6de8c50cdf8ac56652714
jackeir <jackeir@gmail.com>
2015-10-13 16:11:37 +0800

    09 建立 gulpfile.js 並建立 default 與 mytask1 工作 (Task)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
-gulp.task('default', function(){
+var sleep = require('sleep');
+gulp.task('default',['mytask1','mytask2'], function(){
 	console.log('Help, Gulp!');
 });
 
-gulp.task('mytask1', function(){
-	console.log('Help, mytask1\'s Gulp!');
+gulp.task('mytask1', function(cb){
+	console.log('mytask1 !');
+	sleep.sleep(3);
+	console.log('mytask1 wait 3 seconds!');
+	cb();
+});
+
+gulp.task('mytask2', function(cb){
+	//console.log('Help, mytask2\'s Gulp!');
+	//setTimeout(function() {
+	//	console.log('mytask2 wait 3 seconds!');
+	//}, 10000);
+	
+	console.log('mytask2 !');
+	cb();
 });
