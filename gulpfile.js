7a7f92a3c4cca18eda45922df90f659571e95277
jackeir <jackeir@gmail.com>
2015-10-13 16:20:21 +0800

    12 實作 output1, output2 工作，體驗 gulp.src 與 gulp.dest 的使用方法

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,34 @@ gulp.task('default',['mytask1','mytask2'], function(){
 	console.log('Help, Gulp!');
 });
 
-gulp.task('mytask1', function(cb){
+gulp.task('mytask1', function(){
 	console.log('mytask1 !');
 	sleep.sleep(3);
 	console.log('mytask1 wait 3 seconds!');
-	cb();
+	
 });
 
-gulp.task('mytask2', function(cb){
+gulp.task('mytask2', function(){
 	//console.log('Help, mytask2\'s Gulp!');
 	//setTimeout(function() {
 	//	console.log('mytask2 wait 3 seconds!');
 	//}, 10000);
 	
 	console.log('mytask2 !');
-	cb();
+	
+});
+
+
+gulp.task('output1', function(){
+	gulp.src('assets/vendor/bootstrap/**/*.js') //第一個*號是base dir, 也就是bootstrap下的檔案結構會複製
+	.pipe(gulp.dest('output1'));
+});
+
+gulp.task('output2', function(){
+	console.log('exec output2');//復制內容與output1相同, 只不過來源的base folder設在assets/vnedor 所以output2 folder中會建立bootstrap這層資料夾
+	gulp.src('assets/vendor/bootstrap/**/*.js'
+	,{
+		base: 'assets/vendor'
+	}) 
+	.pipe(gulp.dest('output2'));
 });
